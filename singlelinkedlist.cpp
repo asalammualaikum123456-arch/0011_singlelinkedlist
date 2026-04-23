130e081ad9a40e45c0f669efee82f65790aec197
asalammualaikum123456-arch <asalammualaikum123456@gmail.com>
2026-04-23 15:45:26 +0700

    complete addNode method in LinkedList class to handle node insertion in sorted order

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -43,3 +43,23 @@ class LinkedList
         nodeBaru->next =START;
         START = nodeBaru;
         return;
+     }
+
+     Node *previous =START;
+     Node *current = START;
+
+     while (current != NULL && nim > current->noMhs)
+     {
+        if (nim == current->noMhs)
+        {
+cout << "\nDuplikasi noMhs tidak dijinkan\n";
+return;
+        }
+        previous = current;
+        current = current->next;
+
+     }
+     nodeBaru-> next = current;
+     previous->next = nodeBaru;
+    }
+
